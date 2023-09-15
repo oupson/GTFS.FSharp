7b188b8c65be74db1444aeea1c9bbc6ad6e8a9af
oupson <oupson1er@gmail.com>
2023-09-15 09:22:23 +0200

    Initial Commit

---
+++ Program.fs
@@ -0,0 +1,90 @@
+﻿open System
+open System.IO.Compression
+open System.Net.Http
+open System.IO
+
+open FSharp.Data
+open Npgsql
+open Npgsql.FSharp
+
+
+type RouteType =
+    | Tram = 0
+    | Subway = 1
+    | Rail = 2
+    | Bus = 3
+    | Ferry = 4
+    | CableTram = 5
+    | AerialLift = 6
+    | Funicular = 7
+    | TrolleyBus = 11
+    | Monorail = 12
+
+let downloadFile url savePath =
+    async {
+        use client = new HttpClient()
+        let! bytes = client.GetByteArrayAsync(Uri(url)) |> Async.AwaitTask
+        do! File.WriteAllBytesAsync(savePath, bytes) |> Async.AwaitTask
+    }
+
+let truncateTable database table =
+    database
+    |> Sql.existingConnection
+    |> Sql.query $"TRUNCATE %s{table} RESTART IDENTITY CASCADE"
+    |> Sql.executeNonQuery
+    |> ignore
+
+
+let databaseUrl = Environment.GetEnvironmentVariable "DATABASE_CONNECTION_STRING"
+let url = "https://chouette.enroute.mobi/api/v1/datas/keolis_orleans.gtfs.zip"
+let savePath = "/tmp/gtfs.zip"
+
+let dataSourceBuilder = NpgsqlDataSourceBuilder databaseUrl
+dataSourceBuilder.MapEnum<RouteType>("route_type") |> ignore
+let dataSource = dataSourceBuilder.Build()
+
+let connection = dataSource |> Sql.fromDataSource |> Sql.createConnection
+
+downloadFile url savePath |> Async.RunSynchronously
+
+truncateTable connection "ROUTE"
+truncateTable connection "AGENCY"
+
+let f = new FileStream(savePath, FileMode.Open)
+let archive = new ZipArchive(f)
+
+let zipArchiveEntry = archive.GetEntry("agency.txt").Open()
+
+let agencies = CsvFile.Load zipArchiveEntry
+
+for row in agencies.Rows do
+    connection
+    |> Sql.existingConnection
+    |> Sql.query "INSERT INTO AGENCY(agencyId, agencyName, agencyUrl) VALUES (@id,@name,@url)"
+    |> Sql.parameters
+        [ "@id", row.GetColumn("agency_id") |> Sql.text
+          "@name", row.GetColumn("agency_name") |> Sql.text
+          "@url", row.GetColumn("agency_url") |> Sql.text ]
+    |> Sql.executeNonQuery
+    |> ignore
+
+let transaction = connection.BeginTransaction()
+let routes = archive.GetEntry("routes.txt").Open() |> CsvFile.Load
+
+for route in routes.Rows do
+    connection
+    |> Sql.existingConnection
+    |> Sql.query
+        "INSERT INTO ROUTE(routeId, routeShortName, routeLongName, routeType, agencyId) VALUES (@id, @sn, @ln, @ty, @aid)"
+    |> Sql.parameters
+        [ "@id", route.GetColumn("route_id") |> Sql.text
+          "@sn", route.GetColumn("route_short_name") |> Sql.text
+          "@ln", route.GetColumn("route_long_name") |> Sql.text
+          "@ty",
+          NpgsqlParameter(null, enum<RouteType> (int (route.GetColumn("route_type"))))
+          |> Sql.parameter
+          "@aid", route.GetColumn("agency_id") |> Sql.text ]
+    |> Sql.executeNonQuery
+    |> ignore
+
+transaction.Commit()
---
+++ SCHEMA.sql
@@ -0,0 +1,92 @@
+DROP TABLE IF EXISTS VEHICLE;
+DROP TABLE IF EXISTS STOPPING_AT;
+DROP TABLE IF EXISTS TRIP;
+DROP TABLE IF EXISTS STOP;
+DROP TABLE IF EXISTS SHAPE_POINT;
+DROP TABLE IF EXISTS ROUTE;
+DROP TABLE IF EXISTS AGENCY;
+
+DROP TYPE IF EXISTS ROUTE_TYPE;
+DROP TYPE IF EXISTS DIRECTION;
+
+CREATE TABLE AGENCY
+(
+    agencyId   TEXT NOT NULL,
+    agencyName TEXT NOT NULL,
+    agencyUrl  TEXT NULL,
+    CONSTRAINT PK_AGENCY PRIMARY KEY (agencyId)
+);
+
+CREATE TYPE ROUTE_TYPE AS ENUM ('tram', 'subway','rail','bus','ferry','cabletram','aeriallift','funicular','trolleybus','monorail');
+
+CREATE TABLE ROUTE
+(
+    routeId        TEXT       NOT NULL,
+    routeShortName TEXT       NULL,
+    routeLongName  TEXT       NULL,
+    routeType      ROUTE_TYPE NOT NULL,
+    agencyId       TEXT       NOT NULL,
+    CONSTRAINT PK_ROUTE PRIMARY KEY (routeid),
+    CONSTRAINT CHECK_ROUTE_NAME_NOT_NULL CHECK ( ROUTE.routeShortName IS NOT NULL OR ROUTE.routeLongName IS NOT NULL )
+);
+
+CREATE TABLE SHAPE_POINT
+(
+    shapePointIndex    INTEGER                NOT NULL,
+    shapedId           TEXT                   NOT NULL,
+    shapePointLocation GEOGRAPHY(POINT, 4326) NOT NULL,
+    CONSTRAINT PK_SHAPE_POINT PRIMARY KEY (shapedId, shapePointIndex)
+);
+
+CREATE TABLE STOP
+(
+    stopId       TEXT                   NOT NULL,
+    stopName     TEXT                   NOT NULL,
+    stopLocation GEOGRAPHY(POINT, 4326) NOT NULL,
+    CONSTRAINT PK_STOP PRIMARY KEY (stopId)
+);
+
+CREATE TABLE STOPPING_AT
+(
+    tripId                  TEXT      NOT NULL,
+    stopId                  TEXT      NOT NULL,
+    stoppingAtIndex         INTEGER   NOT NULL,
+    stoppingAtPredictedTime TIMESTAMP NULL,
+    stoppingAtRealTime      TIMESTAMP NULL,
+    stoppingAtIsSkipped     BOOLEAN   NOT NULL DEFAULT FALSE,
+    CONSTRAINT PK_STOPPING_AT PRIMARY KEY (tripId, stopId),
+    CONSTRAINT CHECK_STOPPING_AT_HAVE_TIME CHECK ( stoppingAtPredictedTime IS NOT NULL OR stoppingAtRealTime IS NOT NULL )
+);
+
+CREATE TYPE DIRECTION AS ENUM ('0', '1');
+
+CREATE TABLE TRIP
+(
+    tripId         TEXT      NOT NULL,
+    tripHeadSign   TEXT      NULL,
+    tripShortName  TEXT      NULL,
+    tripDirection  DIRECTION NULL,
+    tripIsCanceled BOOLEAN   NOT NULL DEFAULT FALSE,
+    routeId        TEXT      NOT NULL,
+    shapedId       TEXT      NOT NULL,
+    CONSTRAINT PK_TRIP PRIMARY KEY (tripId)
+);
+
+CREATE TABLE VEHICLE
+(
+    vehicleId       TEXT NOT NULL,
+    vehiclePosition GEOMETRY(POINT, 4326),
+    tripId          TEXT NOT NULL,
+    CONSTRAINT PK_VEHICLE PRIMARY KEY (vehicleId)
+);
+
+ALTER TABLE ROUTE
+    ADD CONSTRAINT FK_ROUTE_AGENCY FOREIGN KEY (agencyId) REFERENCES AGENCY (agencyId);
+ALTER TABLE STOPPING_AT
+    ADD CONSTRAINT FK_STOPPING_AT_STOP FOREIGN KEY (stopId) REFERENCES STOP (stopId);
+ALTER TABLE STOPPING_AT
+    ADD CONSTRAINT FK_STOPPING_AT_TRIP FOREIGN KEY (tripId) REFERENCES TRIP (tripId);
+ALTER TABLE TRIP
+    ADD CONSTRAINT FK_TRIP_ROUTE FOREIGN KEY (routeId) REFERENCES ROUTE (routeId);
+ALTER TABLE VEHICLE
+    ADD CONSTRAINT FK_VEHICLE_TRIP FOREIGN KEY (tripId) REFERENCES TRIP (tripId);
