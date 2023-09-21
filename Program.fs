07b992efa6d83db38b82d792791843eb9f5f53ea
oupson <oupson1er@gmail.com>
2023-09-21 09:49:39 +0200

    Add basic gtfs-rt decoding

--- Program.fs
+++ Program.fs
@@ -4,8 +4,10 @@ open System.Net.Http
 open System.IO
 
 open GtfsReader
+open Microsoft.FSharp.Core
 open Npgsql
 open Npgsql.FSharp
+open ProtoBuf
 
 let downloadFile url savePath =
     async {
@@ -14,9 +16,37 @@ let downloadFile url savePath =
         do! File.WriteAllBytesAsync(savePath, bytes) |> Async.AwaitTask
     }
 
+let downloadRealtime url =
+    async {
+        use client = new HttpClient()
+        let! stream = client.GetStreamAsync(Uri(url)) |> Async.AwaitTask
+
+        return Serializer.Deserialize<TransitRealtime.FeedMessage>(stream)
+    }
+
 let databaseUrl = Environment.GetEnvironmentVariable "DATABASE_CONNECTION_STRING"
 let url = Environment.GetEnvironmentVariable "GTFS_URL"
 let savePath = "/tmp/gtfs.zip"
+
+let realtimeUrl = Environment.GetEnvironmentVariable "REALTIME_URL"
+
+let message = downloadRealtime realtimeUrl |> Async.RunSynchronously
+
+printfn $"Is full dataset : %b{message.Header.incrementality = TransitRealtime.FeedHeader.Incrementality.FullDataset}"
+
+for entity in message.Entities do
+    if entity.Alert <> null then
+        printfn
+            "Alert : %s"
+            (entity.Alert.DescriptionText.Translations
+             |> Seq.map (fun r -> r.Text)
+             |> String.concat ",")
+
+    if entity.TripUpdate <> null then
+        printfn $"Trip update : %s{entity.TripUpdate.Trip.TripId}"
+
+    if entity.Vehicle <> null then
+        printfn $"Vehicle position : %s{entity.Vehicle.Vehicle.Id}"
 
 let dataSourceBuilder = NpgsqlDataSourceBuilder databaseUrl
 dataSourceBuilder.MapEnum<RouteType>("route_type") |> ignore
